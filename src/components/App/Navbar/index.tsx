471c00d82cca68a8fef8a9a5e0bee74741ad0e0f
Sercho <diaz.sergio@protonmail.com>
2022-02-25 11:37:51 -0300

    Iniciando rama

--- src/components/App/Navbar/index.tsx
+++ src/components/App/Navbar/index.tsx
@@ -1,33 +1,33 @@
-import styles from './styles.module.css'
-import { Route } from '../routes/routes'
+import styles from './styles.module.css';
+import {Route} from '../routes/routes';
 import Link from './Link';
-import { memo } from 'react';
+import {memo} from 'react';
 
 interface NavbarProps {
-    userRoutes: Route[]
+	userRoutes: Route[];
 }
 
-const Navbar = ({ userRoutes }: NavbarProps) => {
+const Navbar = ({userRoutes}: NavbarProps) => {
+	const buildLinks = () => {
+		return userRoutes
+			.filter((route) => route.isInLayout())
+			.map((route) => (
+				<Link
+					key={route.routeProps.path?.toString()}
+					to={route.routeProps.path}
+					layoutProps={route.layoutProps}
+				/>
+			));
+	};
 
-    const buildLinks = () => {
-        return userRoutes
-            .filter(route => route.isInLayout())
-            .map(route =>
-                <Link
-                    key={route.routeProps.path?.toString()}
-                    to={route.routeProps.path}
-                    layoutProps={route.layoutProps}
-                />
-            );
-    };
+	return (
+		<div className={styles.navbar}>
+			<div className={styles.links}>
+				{buildLinks()}
+				<button>LogOut</button>
+			</div>
+		</div>
+	);
+};
 
-    return (
-        <div className={styles.navbar}>
-            <div className={styles.links}>
-                {buildLinks()}
-            </div>
-        </div>
-    )
-}
-
-export default memo(Navbar)
+export default memo(Navbar);
