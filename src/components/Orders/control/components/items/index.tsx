a7fcb1036657fdf4406f2db684655bbf57145338
Julian Estefan <julianestefan@gearthlogic.com>
2022-03-11 19:03:18 -0300

    Added basic structure for controlling orders

---
+++ src/components/Orders/control/components/items/index.tsx
@@ -0,0 +1,12 @@
+
+
+const CurrentOrdersTable = () => {
+
+    return (
+        <>
+           
+        </>
+    );
+}
+
+export default CurrentOrdersTable;
--- src/components/Orders/control/index.tsx
+++ src/components/Orders/control/index.tsx
@@ -1,20 +1,49 @@
 import moment from "moment";
-import { useMemo } from "react";
+import { Accordion, AccordionSummary, Grid, Paper, Typography } from "@material-ui/core";
+import { AccordionDetails } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-
+import Items from './components/items'
 import useFetch from "../../../hooks/useFetch";
 import { OrderState } from "../../../constants/enums/orderStates.enum";
 import { getOrders } from "../../../services/api/orders";
-import Table from './components/table'
+
+const paperStyles = {
+    minHeight : '85vh',
+    padding: 10,
+    width: '60%'
+}
+const orderRequest = getOrders({ 
+    pickupDate: moment().format("YYYY-MM-DD"),
+    state: [OrderState.Controlling]
+});
 
 function ControllingOrderList() {
-    const { data } = useFetch<{orders: any[]}>(useMemo(() => getOrders({
-        pickupDate: moment().format("YYYY-MM-DD"),
-        state: [OrderState.Controlling]
-    }), []))
-    debugger
+    const { data } = useFetch<{ orders: any[] }>( orderRequest )
+    
     return (
-        <Table orders={data?.orders} />
+        <Grid container  justifyContent="center">
+            <Paper style={paperStyles}>
+                <Typography textAlign="center" variant="h4"  >
+                    Ordenes por controlar
+                </Typography>
+                {(data?.orders?.length || 0 )> 0 ?
+                    data?.orders.map(order => (
+                        <Accordion key={order.orderId}>
+                            <AccordionSummary           
+                            expandIcon={<ExpandMoreIcon />}>
+                                {order.client.alias}
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Items />
+                            </AccordionDetails>
+                        </Accordion>
+                    )) : <Typography textAlign="center"   >
+                       No hay ordenes por controlar
+                    </Typography>
+                }
+            </Paper>
+        </Grid>
     )
 }
 
